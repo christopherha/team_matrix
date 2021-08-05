d93f5f86553859c1602d06a8758f6bdf938d2e46
goswami51 <goswami.ami@gmail.com>
2021-08-05 19:47:31 +0000

    Added component structure.

--- matrix-ui/src/App.js
+++ matrix-ui/src/App.js
@@ -1,5 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+//Search
+// 1. page -> display student list
+// 2. page -> display course List
+
+//3. page - add student
+//4.Enroll Student to a course
+    //Search by Student ==> it display the list of courses
+    // Enroll button => display the remaining course => then user can add the student to any of the displayed course.
+//Enroll
+
 
 function App() {
   return (
