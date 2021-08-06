48d7f83fe54c896f9ac698e6e518596a596d25b8
ajcolby10 <ajcolby10@gmail.com>
2021-08-06 17:25:26 +0000

    Removing Find all Courses.

--- matrix-ui/src/components/PageHeader/Menu.js
+++ matrix-ui/src/components/PageHeader/Menu.js
@@ -4,8 +4,7 @@ import {Link} from "react-router-dom";
 const Menu = () => {
     return (
         <ul className="nav">
-            <li><Link to="/find">Find Student</Link></li>
-            <li><Link to="/find">Find Course</Link></li>
+            <li><Link to="/find">Show All Students</Link></li>
             <li><Link to="/addStudent">Add Student</Link></li>
             <li><Link to="/addCourse">Add Course</Link></li>
         </ul>
